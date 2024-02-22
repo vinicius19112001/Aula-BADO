520e3c6df98ecd5575c38d260fab38d42f6c7d79
vinicius19112001 <142223428+vinicius19112001@users.noreply.github.com>
2024-02-22 08:54:51 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var pessoa = "832.192.304-34"
+
+console.log(`Conteúdo inicial da variavel: ${pessoa} | TIpo: ${typeof(pessoa)}`);
+
+pessoa = 259
+
+console.log(`Mudei o COnteúdo: ${pessoa} | Tipo: ${typeof(pessoa)}`)
